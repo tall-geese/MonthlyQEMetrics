53d4908712a07abfe10148d5e56d1df5faf76a99
xyzMark <dieckman7461@gmail.com>
2021-04-05 18:49:20 -0400

    Initial Commit

---
+++ Jupyter/sql/LeadingMonth.sql
@@ -0,0 +1,18 @@
+--Part 1  (Unresolved quantites from the previous month)
+--Group by Job for the Part 2 and 3 Union. Inner joing with laborEntry, select all non-discrep quantities
+SELECT ld2.JobNum, ld2.DiscrepQty, ld2.CreateDate
+FROM EpicorLive10.dbo.LaborDtl ld2 
+INNER JOIN 
+		(SELECT	src.JobNum[JobNum], SUM(src.DiscrepQty)[DiscrepQty] 
+	FROM (SELECT ld.JobNum, ld.DiscrepQty
+			FROM EpicorLive10.dbo.LaborDtl ld 
+			WHERE ld.CreateDate > '2021-03-01' and ld.Company = 'JPMC' and ld.DiscrepQty <> 0
+			UNION ALL
+			SELECT dh.JobNum, da.Quantity
+			FROM EpicorLive10.dbo.DMRActn da
+			LEFT OUTER JOIN EpicorLive10.dbo.DMRHead dh on da.DMRNum = dh.DMRNum 
+			WHERE da.ActionDate > '2021-03-01' and (da.ActionType = 'R' OR da.DestinationType = 'M') and da.JobNum <> '' and da.Company = 'JPMC' and dh.JobNum <> '') src
+	GROUP BY src.JobNum) src2
+ON ld2.JobNum  = src2.JobNum
+WHERE ld2.CreateDate <= '2021-03-01' AND ld2.DiscrepQty <> 0
+ORDER BY ld2.JobNum ASC
---
+++ Jupyter/sql/MonthlyActivity.sql
@@ -0,0 +1,13 @@
+--Part 2 (Discrep Qty action over the given month)
+--Select from the jobs occuring over a given month that have a non-zero discrep qty but a sum total qty of 0
+SELECT ld.JobNum, ld.DiscrepQty, ld.CreateDate
+FROM EpicorLive10.dbo.LaborDtl ld 
+WHERE ld.CreateDate > '2021-03-01' and ld.Company = 'JPMC' and ld.DiscrepQty <> 0
+UNION ALL
+--Part3
+--Select from DMRs where we either accepted parts as job material or we failed parts
+SELECT dh.JobNum, (-da.Quantity), da.ActionDate
+FROM EpicorLive10.dbo.DMRActn da
+LEFT OUTER JOIN EpicorLive10.dbo.DMRHead dh on da.DMRNum = dh.DMRNum 
+WHERE da.ActionDate > '2021-03-01' and (da.ActionType = 'R' OR da.DestinationType = 'M') and da.JobNum <> '' and da.Company = 'JPMC' and dh.JobNum <> ''
+ORDER BY ld.CreateDate  ASC
